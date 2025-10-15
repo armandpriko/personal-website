2979be21589ca6768ead419181f351acbed1f263
Dzudzogbe Prince Armand, KOUMI <koumiprince@gmail.com>
2025-10-15 15:09:51 +0300

    �� Fix: Configure Astro base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://armandpriko.github.io',
+  base: '/personal-website',
   integrations: [tailwind()],
   output: 'static',
 });
